6b5fec010ec4568d00ca4a2bbcf16137845b395e
carlos <carlos@DESKTOP-D73TIV5>
2021-11-29 21:29:33 -0600

    Se genero la rama de carlos

--- src/datos/DatosDesordenados.java
+++ src/datos/DatosDesordenados.java
@@ -1,6 +1,7 @@
 /*Clase que contien los algoritmos de ordenamiento*/
 package datos;
 
+import java.util.Comparator;
 import static programas.QuickSort.Quicksort;
 
 /**
@@ -71,6 +72,27 @@ public class DatosDesordenados {
         // enviamos los datos ordenados a la clase DatosOrdenados colocamos los datos que estan en el arreglo en la clase datos ordenados
         return new DatosOrdenados(arreglo);
     }
+     
+     public DatosOrdenados radixSort(){
+        int [] arreglo = this.getCopiaValores();
+        int n = arreglo.length;
+        int cont = 0;
+        int ci = 0;
+        for (int i=0; i < n-1; i++){
+            for (int j=0; j < n-i-1; j++){
+                cont++;
+                if (arreglo[j] < arreglo[j+1]){
+                    // intercambiar: swap
+                    ci++;
+                    int temporal = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = temporal;
+                }
+            }
+        }
+        return new DatosOrdenados(arreglo);
+    }
+     
      
      public DatosOrdenados ShellSort(){
         
--- src/programas/TestOrdenamiento.java
+++ src/programas/TestOrdenamiento.java
@@ -8,77 +8,92 @@ package programas;
 import datos.DatosDesordenados;
 import datos.DatosOrdenados;
 import javax.swing.JOptionPane;
+
 public class TestOrdenamiento {
+
     public static void main(String[] args) {
         //Declaracion de variables:
         DatosDesordenados m;
         int n, s;
         long t, t1, t2;
-        
+
         //Pedimos los datos: 
         n = Integer.parseInt(JOptionPane.showInputDialog(
                 "¿Cuántos datos son?"));
         s = Integer.parseInt(JOptionPane.showInputDialog(
                 "¿Cuál es el valor máximo?"));
-        
+
         //Construimos el arreglo de datos desordenados con los parametros pedidos n, s:
         m = new DatosDesordenados(s, n);
-        
+
         //Los datos los guardamos en la variable m y lo enviamos al metod que genra los datos aleatorios
         m.generarValores();
+
+        //Imprimimos los datos desordenados:
+        System.out.println("" + m);
+        System.out.println("");
+
+        System.out.println("Ordenamiento por burbuja:");
+        t1 = System.nanoTime();//<-- Star
+        DatosOrdenados mob = m.bubbleSort();//<-- Ordenamos
+        t2 = System.nanoTime(); //<-- Stop
+        t = t2 - t1;
+        System.out.println("n=" + n);
+        System.out.println(mob);
+        System.out.println("tiempo= " + t + "ns");
+        System.out.println("");
+
+        System.out.println("Ordenamiento por Insercion:");
+        t1 = System.nanoTime();//<-- Star
+        DatosOrdenados moi = m.insertSort();//<-- Ordenamos
+        t2 = System.nanoTime(); //<-- Stop
+        t = t2 - t1;
+        System.out.println("n=" + n);
+        System.out.println(moi);
+        System.out.println("tiempo= " + t + "ns");
+        System.out.println("");
+
+        System.out.println("Ordenamiento por mezcla:");
+        t1 = System.nanoTime();//<-- Star
+        DatosOrdenados mog = m.mergeSort();//<-- Ordenamos
+        t2 = System.nanoTime(); //<-- Stop
+        t = t2 - t1;
+        System.out.println("n=" + n);
+        System.out.println(mob);
+        System.out.println("tiempo= " + t + "ns");
+        System.out.println("");
+
+        System.out.println("Ordenamiento por Quicksort:");
+        t1 = System.nanoTime();//<-- Star
+        DatosOrdenados moq = m.Quicksort();//<-- Ordenamos
+        t2 = System.nanoTime(); //<-- Stop
+        t = t2 - t1;
+        System.out.println("n=" + n);
+        System.out.println(mob);
+        System.out.println("tiempo= " + t + "ns");
+        System.out.println("");
+
+        System.out.println("Ordenamiento por ShellSort:");
+        t1 = System.nanoTime();//<-- Star
+        DatosOrdenados mos = m.ShellSort();//<-- Ordenamos
+        t2 = System.nanoTime(); //<-- Stop
+        t = t2 - t1;
+        System.out.println("n=" + n);
+        System.out.println(mob);
+        System.out.println("tiempo= " + t + "ns");
+        System.out.println("");
         
-            //Imprimimos los datos desordenados:
-            System.out.println("" + m);
-            System.out.println("");
-            
-            System.out.println("Ordenamiento por burbuja:");
+        System.out.println("Ordenamiento por Radix:");
             t1 = System.nanoTime();//<-- Star
-            DatosOrdenados mob = m.bubbleSort();//<-- Ordenamos
+            DatosOrdenados mor = m.radixSort();//<-- Ordenamos
             t2 = System.nanoTime(); //<-- Stop
             t= t2 - t1;
             System.out.println("n="+ n);
             System.out.println(mob);
             System.out.println("tiempo= "+t+"ns");
-            System.out.println("");
-            
-            System.out.println("Ordenamiento por Insercion:");
-            t1 = System.nanoTime();//<-- Star
-            DatosOrdenados moi = m.insertSort();//<-- Ordenamos
-            t2 = System.nanoTime(); //<-- Stop
-            t= t2 - t1;
-            System.out.println("n="+ n);
-            System.out.println(moi);
-            System.out.println("tiempo= "+t+"ns");
-            System.out.println("");
-            
-            System.out.println("Ordenamiento por mezcla:");
-            t1 = System.nanoTime();//<-- Star
-            DatosOrdenados mog = m.mergeSort();//<-- Ordenamos
-            t2 = System.nanoTime(); //<-- Stop
-            t= t2 - t1;
-            System.out.println("n="+ n);
-            System.out.println(mob);
-            System.out.println("tiempo= "+t+"ns");
-            System.out.println("");
-           
-            System.out.println("Ordenamiento por Quicksort:");
-            t1 = System.nanoTime();//<-- Star
-            DatosOrdenados moq = m.Quicksort();//<-- Ordenamos
-            t2 = System.nanoTime(); //<-- Stop
-            t= t2 - t1;
-            System.out.println("n="+ n);
-            System.out.println(mob);
-            System.out.println("tiempo= "+t+"ns");
-            System.out.println("");
-            
-            System.out.println("Ordenamiento por ShellSort:");
-            t1 = System.nanoTime();//<-- Star
-            DatosOrdenados mos = m.ShellSort();//<-- Ordenamos
-            t2 = System.nanoTime(); //<-- Stop
-            t= t2 - t1;
-            System.out.println("n="+ n);
-            System.out.println(mob);
-            System.out.println("tiempo= "+t+"ns");
+
     }
-    
 }
+    
+
+
